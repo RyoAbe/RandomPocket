ece7b9f75a94d6b3b1cb917c70c44a59db16091b
RyoAbe <ryoabe.gm@gmail.com>
2014-03-17 23:44:41 +0900

    各UIの色を修正

--- RandomPocket/Classes/UI/RPColor.m
+++ RandomPocket/Classes/UI/RPColor.m
@@ -10,10 +10,10 @@
 
 @implementation RPColor
 
-+ (UIColor*)NavigationBarColor      { return [UIColor colorWithRed:0.5f green:0.5f blue:0.5f alpha:1.f]; }
-+ (UIColor*)NavigationBarTitleColor { return [UIColor colorWithRed:0.3f green:0.3f blue:0.3f alpha:1.f]; }
-+ (UIColor*)BarButtonItemColor      { return [UIColor colorWithRed:0.6f green:0.6f blue:0.6f alpha:1.f]; }
-+ (UIColor*)ToolButtonItemColor     { return [UIColor colorWithRed:0.6f green:0.6f blue:0.6f alpha:1.f]; }
++ (UIColor*)NavigationBarColor      { return [UIColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.f]; }
++ (UIColor*)NavigationBarTitleColor { return [UIColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.f]; }
++ (UIColor*)BarButtonItemColor      { return [UIColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.f]; }
++ (UIColor*)ToolButtonItemColor     { return [UIColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.f]; }
 + (UIColor*)PocketListCellMoreButtonColor    { return [UIColor colorWithRed:0.6f green:0.6f blue:0.6f alpha:1.f]; }
 + (UIColor*)PocketListCellArchiveButtonColor { return [UIColor colorWithRed:0.18f green:0.67f blue:0.84f alpha:1.0f]; }
 @end
